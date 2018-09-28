2c02937150fd76559626692562004a309c2c4ded
DESKTOP-D3TA66Q\konstantinz <konstantin.zheleznov@nurd.com>
2018-09-28 11:57:17 +0300

    TaskPool test renovated

--- Templates/Templates/src/TaskPool/TaskPoolTest.cpp
+++ Templates/Templates/src/TaskPool/TaskPoolTest.cpp
@@ -8,7 +8,7 @@
 
 std::mutex mut;
 
-int testFun(int i)
+int fastFun(int i)
 {
    mut.lock();
    std::cout << "task " << i << std::endl;
@@ -16,58 +16,42 @@ int testFun(int i)
 
    return i;
 }
+
+int slowFun(int i)
+{
+   int N = 10 * i;
+
+   for (int n = 0; n < N; ++n)
+   {
+      mut.lock();
+      std::cout << n << std::endl;
+      mut.unlock();
+   }
+
+   return i;
+}
+
+
 void TaskPoolTest()
 {
    TaskPool pool(8);
    std::vector<std::future<int>> results;
 
-   //for (int i = 0; i < 8; ++i)
-   //{
-   //   results.emplace_back(
-   //      pool.enqueue([i]
-   //   {
-   //      //throw 1;
-
-   //      std::cout << "hello " << i << std::endl;
-   //      std::this_thread::sleep_for(std::chrono::seconds(i));
-   //      std::cout << "world " << i << std::endl;
-   //      return i;
-   //   })
-   //   );
-   //}
-   
-
    for (int i = 0; i < 8; ++i)
    {
-      //auto fut = pool.enqueue([i]
-      //{
-      //   //throw 1;
 
-      //   std::cout << "hello " << i << std::endl;
-      //   std::this_thread::sleep_for(std::chrono::seconds(i));
-      //   std::cout << "world " << i << std::endl;
-      //   return i;
-      //});
+      if (i == 1)
+         results.push_back( pool.enqueue(slowFun, i) );
+      else
+         results.push_back( pool.enqueue(fastFun, i) );
 
-      //auto lmd = [&]
-      //{
-      //   //throw 1;
-
-      //   mut.lock();
-      //   std::cout << "task " << i << std::endl;
-      //   mut.unlock();
-      //   return i;
-      //};
-      //auto fut = pool.enqueue(lmd);
-
-      auto fut = pool.enqueue(testFun, i);
-
-      results.push_back(std::move(fut));
+      /*auto fut = pool.enqueue(fastFun, i);
+      results.push_back(std::move(fut));*/
    }
 
-   //for (auto && result : results)
-   //   std::cout << result.get() << ' ';
-   //std::cout << std::endl;
+   for (auto && result : results)
+      std::cout << result.get() << ' ';
+   std::cout << std::endl;
 
    _getch();
 }
