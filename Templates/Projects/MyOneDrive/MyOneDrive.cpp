41c9b5c44eb65bdbefbfe9a5fa42ae2a4e3a0129
DESKTOP-D3TA66Q\konstantinz <konstantin.zheleznov@nurd.com>
2018-11-28 14:45:20 +0200

    MyOneDrive project was added

---
+++ Templates/Projects/MyOneDrive/MyOneDrive.cpp
@@ -0,0 +1,13 @@
+// MyOneDrive.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <vld.h>
+
+#include <conio.h>
+#include <iostream>
+
+int main()
+{
+    _getch();
+
+    return{};
+}
